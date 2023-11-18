5446626402350633aef3c5ebb9a7aad91b465e99
Quach Nam Luong <103545178+QuachNamLuong@users.noreply.github.com>
2023-11-18 16:02:47 +0700

    Update SQLQuery_BTL_HTPT.sql

--- SQLQuery/SQLQuery_BTL_HTPT.sql
+++ SQLQuery/SQLQuery_BTL_HTPT.sql
@@ -1,7 +1,28 @@
-/*CREATE DATABASE [BTL_HTPT]
+USE master
 GO
 
-USE [BTL_HTPT]
-GO*/
+CREATE DATABASE EmployeeManagement
+GO
 
+USE EmployeeManagement
+GO
 
+CREATE TABLE Employee(
+	EmployeeID INT IDENTITY(1, 1),
+	FullName NVARCHAR(100),
+	PhoneNo CHAR(15) UNIQUE,
+	Birthday DATE,
+	Adress NVARCHAR(100),
+	CONSTRAINT PK_Employee PRIMARY KEY (EmployeeID)
+)
+GO
+
+CREATE TABLE History(
+	EmployeeID INT,
+	UpdateTime DATETIME,
+	SiteUpdate NVARCHAR(100),
+	ContentUpdate NTEXT,
+	CONSTRAINT PK_History PRIMARY KEY (EmployeeID, UpdateTime),
+	CONSTRAINT FK_History_Employee FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID)
+)
+GO
