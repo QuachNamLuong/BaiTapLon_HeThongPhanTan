ae2e4e8888104d57eecdd999f53a629746e44498
Quach Nam Luong <103545178+QuachNamLuong@users.noreply.github.com>
2023-12-06 09:18:39 +0700

    Add SP_UpdateEmpoyee

--- SQLQuery/SQLQuery_BTL_HTPT.sql
+++ SQLQuery/SQLQuery_BTL_HTPT.sql
@@ -26,3 +26,21 @@ BEGIN
 END
 GO
 
+CREATE PROCEDURE SP_UpdateEmployee
+    @EmployeeID INT
+    @FullName NVARCHAR(100),
+    @PhoneNo CHAR(15),
+    @Birthday DATE,
+    @Salary DECIMAL(10, 2),
+AS
+BEGIN
+    UPDATE Employee
+    SET
+	FullName = CASE WHEN @FullName IS NOT NULL THEN @FullName ELSE FullName END,
+	PhoneNo = CASE WHEN @PhoneNo IS NOT NULL THEN @PhoneNo ELSE PhoneNo END,
+	Birthday = CASE WHEN @Birthday IS NOT NULL THEN @Birthday ELSE Birthday END,
+	Salary = CASE WHEN @Salary IS NOT NULL THEN @Salary ELSE Salary END,
+    WHERE EmployeeID = @EmployeeID;
+END
+GO
+
